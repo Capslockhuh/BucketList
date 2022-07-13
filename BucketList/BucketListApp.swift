6fb5e7d7a29c8b91726059a574a25419007941a0
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-13 09:24:43 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jan Andrzejewski on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
