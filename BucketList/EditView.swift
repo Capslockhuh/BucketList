b71b4c5206bd3e88f486bce901bf0e3747a96fce
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-15 12:45:13 +0200

    Added a function that runs when a marker is tapped - it fetches data from Wikipedia and shows interesting places near the marker

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -62,8 +62,34 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            // fail
+            loadingState = .failed
+        }
+    }
+    
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
         self.onSave = onSave
