dca9c1f3633fa74b4ce01027911a6c1e1dcc4d34
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-15 12:52:01 +0200

    Made Page struct conform to Comparable

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -82,7 +82,7 @@ struct EditView: View {
             let items = try JSONDecoder().decode(Result.self, from: data)
             
             // success
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             // fail
