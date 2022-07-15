1c779bf6c12c41d34c3dc87f98fc975f2f772aa0
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-15 10:02:08 +0200

    Added a custom marker view and simplified location's coordinate property

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
@@ -13,4 +14,8 @@ struct Location: Identifiable, Codable, Equatable {
     let description: String
     let latitude: Double
     let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 }
