36d34e000ab0c0e89a8a6ef10cd70a2d101c837d
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-07-16 16:36:34 +0200

    Refactored EditView to use MVVM as well

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -11,34 +11,24 @@ struct EditView: View {
     
     @Environment(\.dismiss) var dismiss
     
-    var location: Location
-    
-    @State private var name: String
-    @State private var description: String
-    
     var onSave: (Location) -> Void
     
-    enum LoadingState {
-        case loading, loaded, failed
-    }
-    
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
-    
+    @StateObject var editViewModel: EditViewModel
+        
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Place name", text: $name)
-                    TextField("Description", text: $description)
+                    TextField("Place name", text: $editViewModel.name)
+                    TextField("Description", text: $editViewModel.description)
                 }
                 
                 Section("Nearby...") {
-                    switch loadingState {
+                    switch editViewModel.loadingState {
                     case .loading:
                         Text("Loading...")
                     case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
+                        ForEach(editViewModel.pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
@@ -53,49 +43,25 @@ struct EditView: View {
             .navigationTitle("Place details")
             .toolbar {
                 Button("Save") {
-                    var newLocation = location
+                    var newLocation = editViewModel.location
                     newLocation.id = UUID()
-                    newLocation.name = name
-                    newLocation.description = description
+                    newLocation.name = editViewModel.name
+                    newLocation.description = editViewModel.description
 
                     onSave(newLocation)
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await editViewModel.fetchNearbyPlaces()
             }
         }
     }
     
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            let items = try JSONDecoder().decode(Result.self, from: data)
-            
-            // success
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            // fail
-            loadingState = .failed
-        }
-    }
-    
     init(location: Location, onSave: @escaping (Location) -> Void) {
-        self.location = location
+        _editViewModel = StateObject(wrappedValue: EditViewModel(location: location))
+        
         self.onSave = onSave
-
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
     }
 }
 
